867c8e26ddcd4f5f6f200f5109666a71af2f6509
Rithik Jain <wizardofdevops@gmail.com>
2025-05-19 12:38:57 +0000

    commit from act

--- ascii.sh
+++ ascii.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo "-------------------------------------------------------------"
 echo "lets install cowasay"
-sudo apt-get install cowsay -y
+apt-get update > /dev/null 2>&1
+apt-get install -y wget > /dev/null 2>&1
+wget http://archive.ubuntu.com/ubuntu/pool/universe/c/cowsay/cowsay_3.03+dfsg2-8_all.deb > /dev/null 2>&1
+apt-get install -y libtext-charwidth-perl perl > /dev/null 2>&1
+dpkg -i cowsay_3.03+dfsg2-8_all.deb > /dev/null 2>&1
+sleep 10
 echo "-------------------------------------------------------------"
 echo " Lets draw a dragon"
-cowsay -f dragon "Run for cover,I am a DRAGON....RAWR" 
+/usr/games/cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" > dragon.txt
 echo "-------------------------------------------------------------"
 echo " Lets show them if the file is created"
 grep -i "dragon" dragon.txt
